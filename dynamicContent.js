86d899b567cfa61d956cfb026c081f91c3d472bf
nodexs0 <nodexs@outlook.com>
2024-05-01 11:37:24 -0600

    Creacion de handlebar dinamico para la ruta /, ademas de vistas para los codigos 404 y 500

---
+++ dynamicContent.js
@@ -0,0 +1,32 @@
+export const dynamicContent = {
+    'pollo.jpg': {
+        name: 'Tamal de Pollo',
+        path: '/images/pollo.jpg',
+        url: 'https://www.recetasnestlecam.com/recetas/tamal-de-pollo',
+        message: 'Son como el amigo confiable y reconfortante que siempre está ahí cuando lo necesitas, con su relleno de pollo jugoso y su salsa sabrosa.'
+    },
+    'mole.jpg': {
+        name: 'Tamal de Mole',
+        path: '/images/mole.jpg',
+        url: 'https://www.recetasnestle.com.mx/recetas/tamales-mole-chocolate',
+        message: 'Son sofisticados y elegantes, como un amante de las artes culinarias que aprecia los sabores complejos y profundos.'
+    },
+    'rajas.jpg': {
+        name: 'Tamal de Rajas con Queso',
+        path: '/images/rajas.jpg',
+        url: 'https://www.recepedia.com/es-mx/receta/vegetales/218094-tamales-de-rajas-con-queso/',
+        message: 'Tienen un equilibrio perfecto entre lo picante del chile y lo suave del queso, como una pareja que se complementa y se apoya mutuamente.'
+    },
+    'dulce.jpg': {
+        name: 'Tamal Dulce',
+        path: '/images/dulce.jpg',
+        url: 'https://www.recetasnestle.com.mx/recetas/tamales-dulces',
+        message: 'Son juguetones y llenos de alegría, como un niño que encuentra diversión en cada bocado y siempre tiene una sonrisa en el rostro.'
+    },
+    'oaxaqueños.jpg': {
+        name: 'Tamales Oaxaqueños',
+        path: '/images/oaxaqueños.jpg',
+        url: 'https://www.recetasgratis.net/receta-de-tamales-oaxaquenos-receta-original-76245.html',
+        message: 'Son eclécticos y llenos de sorpresas, como un espíritu libre que no se adhiere a las reglas y disfruta explorando nuevas experiencias culinarias.'
+    },
+};
